3a49aa588a8c06d9c2dbf02ba20a357bff22dfcb
Ting <yueteen841216@gmail.com>
2022-04-14 16:51:28 +0800

    service創建

---
+++ src/main/java/com/eeit40/springbootproject/service/ReservationStoreService.java
@@ -0,0 +1,14 @@
+package com.eeit40.springbootproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.eeit40.springbootproject.dao.ReservationStoreRepository;
+
+public class ReservationStoreService {
+
+	@Autowired
+	private ReservationStoreRepository ReservationStoreDao;
+
+	
+	
+}
