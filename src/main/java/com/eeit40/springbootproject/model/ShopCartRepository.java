0f9aa84db2d372e89852fae972f4cc13e0068f07
Angus <Traverser04@gmail.com>
2022-04-19 15:05:19 +0800

    AAA

--- src/main/java/com/eeit40/springbootproject/model/ShopCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eeit40.springbootproject.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopCartRepository  extends JpaRepository<shopLiquor, Integer>{
-	
-//	public shopCart findFirstByOrderByAddedDesc();
-	
-}
