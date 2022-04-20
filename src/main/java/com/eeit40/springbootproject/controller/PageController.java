6e47bd080ef911a8ac0cdc81944afc71f946d072
milkyeway <namilkywaw@gmail.com>
2022-04-20 21:21:16 +0800

    Thymeleaf 測試
    1. pom.xml (78~82)
    2. resources/template(html)
    3. resources/static(css/js/images)
    4. controller/WebController (首頁配置)

--- src/main/java/com/eeit40/springbootproject/controller/PageController.java
+++ src/main/java/com/eeit40/springbootproject/controller/PageController.java
@@ -1,15 +1,15 @@
 package com.eeit40.springbootproject.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class PageController {
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+//	@GetMapping("/")
+//	public String index() {
+//		return "index";
+//	}
 	 
 }
  
