cfe0e2444866b9812ef4348387547be2ae5b185a
Angus <Traverser04@gmail.com>
2022-04-27 18:06:16 +0800

    jsp更改

---
+++ src/main/java/com/eeit40/springbootproject/controller/ShopOrderController.java
@@ -0,0 +1,63 @@
+package com.eeit40.springbootproject.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eeit40.springbootproject.dao.ShopOrderRepository;
+import com.eeit40.springbootproject.model.ShopOrder;
+
+@RestController
+public class ShopOrderController {
+	
+	@Autowired
+	private ShopOrderRepository dao;
+	
+	
+	public ShopOrder insertPost(@RequestBody ShopOrder order) {
+		ShopOrder resOrder = dao.save(order);
+		
+		return resOrder;
+	}
+	
+	@GetMapping(value = "/ShopOrder/get/{postId}")
+	public ShopOrder findPostById(@PathVariable Integer postid) {
+		
+		Optional<ShopOrder> responsePost = dao.findById(postid);
+		
+		if(responsePost.isPresent()) {
+			return responsePost.get();
+		}
+		return null;
+	}
+	
+	@GetMapping(value = "/ShopOrder/get")
+	public ShopOrder findPostById2(@RequestParam Integer postid) {
+		
+		Optional<ShopOrder> responsePost = dao.findById(postid);
+		
+		if(responsePost.isPresent()) {
+			return responsePost.get();
+		}
+		return null;
+	}
+	
+	@GetMapping(value = "/ShopOrder/page/{pageNumber}")
+	public List<ShopOrder> findByPage(@PathVariable Integer pageNumber){
+		
+		Pageable pgb = PageRequest.of(pageNumber-1, 2,Sort.Direction.DESC,"postID");
+		Page<ShopOrder> page = dao.findAll(pgb);
+		List<ShopOrder> list = page.getContent();
+		return list;
+	}
+}
