2ce131ede8eaaf16f8cb6b84d9d41d267b9f673c
Angus <Traverser04@gmail.com>
2022-04-27 15:04:21 +0800

    controller 變更

--- src/main/java/com/eeit40/springbootproject/controller/ShopCartCorntroller.java
+++ src/main/java/com/eeit40/springbootproject/controller/ShopCartCorntroller.java
@@ -3,7 +3,6 @@ package com.eeit40.springbootproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 
@@ -16,6 +15,6 @@ public class ShopCartCorntroller {
 	    public String cartPage() {
 	        return "cart";
 	    }
-
+	 
 	
 }
