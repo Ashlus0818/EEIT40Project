b67d5c2592f4e230cc979af7d87e3291fcab68fb
yueteen <yueteen841216@gmail.com>
2022-05-04 16:40:17 +0800

    fix frontapi

--- src/main/java/com/eeit40/springbootproject/controller/FrontApi.java
+++ src/main/java/com/eeit40/springbootproject/controller/FrontApi.java
@@ -1,25 +1,61 @@
 package com.eeit40.springbootproject.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+//import org.json.JSONObject;
+
 
 @RestController
 public class FrontApi {
 	private final String port = "8090";
 	
+	
 	@CrossOrigin("*")
-	@PostMapping(value="/test")
-	public List<String> test(){
+	
+	@PostMapping(value = "/test")
+	// public List<String> test(@RequestParam("message") String string){
+	public List<String> test(HttpServletRequest request) throws IOException {
 		List<String> list = new ArrayList<>();
-		list.add(0,"name");
-		list.add(1,"num");
+		list.add(0, "name");
+		list.add(1, "num");
+
+//		JSONObject jObj = new JSONObject(request.getParameter(""));
+		System.out.println(request.getParameter("message"));
+		// String body = IOUtils.toString(request.getInputStream(),
+		// StandardCharsets.UTF_8);
+
 		
-		return list;		
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+		String json = "";
+		if (br != null) {
+			json = br.readLine();
+			System.out.println(json);
+		}
+
+//		System.out.println();
+//		System.out.println(list);
+		return list;
 	}
+
+//	@PostMapping("/test")
+//	public String getJson(@RequestParam("message") String string) throws IOException {
+//		System.out.println(string);
+//		return null;
+//	}
 	
 }
